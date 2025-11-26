4c895cdb9517db5433908f9aa711da087f68126e
Vishal Bhingarde <bhingardevishal5@gmail.com>
2025-11-26 09:49:50 +0530

    Add files via upload

---
+++ Sorting and Searching Technique/searching/binary_search_recursive.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearchRecursive(const vector<int>& arr, int left, int right, int target) {
+    if (left > right) return -1;
+    int mid = left + (right - left) / 2;
+    if (arr[mid] == target) return mid;
+    else if (arr[mid] > target)
+        return binarySearchRecursive(arr, left, mid - 1, target);
+    else
+        return binarySearchRecursive(arr, mid + 1, right, target);
+}
