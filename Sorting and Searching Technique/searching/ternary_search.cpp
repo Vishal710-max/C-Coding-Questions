c41ec78be262a6006b5ab1b555155ec160ba6bb0
Vishal Bhingarde <bhingardevishal5@gmail.com>
2025-11-28 14:49:42 +0530

    Add files via upload

---
+++ Sorting and Searching Technique/searching/ternary_search.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int ternarySearch(const vector<int>& arr, int left, int right, int target) {
+    while (left <= right) {
+        int mid1 = left + (right - left) / 3;
+        int mid2 = right - (right - left) / 3;
+        if (arr[mid1] == target) return mid1;
+        if (arr[mid2] == target) return mid2;
+        if (target < arr[mid1]) right = mid1 - 1;
+        else if (target > arr[mid2]) left = mid2 + 1;
+        else left = mid1 + 1, right = mid2 - 1;
+    }
+    return -1;
+}
